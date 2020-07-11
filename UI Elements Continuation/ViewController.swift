07d3dde3fd464017d43509ff8dc3853dbe91ce3d
Eugene Kiseleff <zhenya-kiselev-97@mail.ru>
2020-07-11 15:11:38 +0300

    UITextView. Скрытие клавиатуры по нажатию за пределами зоны редактирования Text View. Подсчет и ограничение вводимых символов. Поднятие зоны редактирования выше клавиатуры.

--- UI Elements Continuation/ViewController.swift
+++ UI Elements Continuation/ViewController.swift
@@ -10,11 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textView.delegate = self
+        
+        textView.text = ""
+        
+        textView.font = UIFont(name: "AppleSDGothicNeo-Light", size: 17)
+        textView.backgroundColor = self.view.backgroundColor
+        textView.layer.cornerRadius = 10
+        
+        NotificationCenter.default.addObserver(self,
+                                       selector: #selector(updateTextView(notification:)),
+                                       name: UIResponder.keyboardWillChangeFrameNotification,
+                                       object: nil)
+
+        NotificationCenter.default.addObserver(self,
+                                       selector: #selector(updateTextView(notification:)),
+                                       name: UIResponder.keyboardWillHideNotification,
+                                       object: nil)
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        self.view.endEditing(true)
+        
+    }
+    
+    @objc func updateTextView(notification: Notification) {
+        
+        guard let userInfo = notification.userInfo as? [String: AnyObject],
+            let keyBoardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            else { return }
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = UIEdgeInsets.zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0,
+                                                 left: 0,
+                                                 bottom: keyBoardFrame.height - bottomConstraint.constant ,
+                                                 right: 0)
+            
+            textView.scrollRangeToVisible(textView.selectedRange)
+        }
+        
+    }
 
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.backgroundColor = .white
+        textView.textColor = .darkGray
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        textView.backgroundColor = self.view.backgroundColor
+        textView.textColor = .black
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        countLabel.text = "\(textView.text.count)"
+        return textView.text.count + (text.count - range.length) <= 60
+    }
 }
 
